56a9a4f5155dfd2b0ab73934ba13ec7cc1933a0f
RedStand <13810063423@126.com>
2019-04-17 21:11:23 +0800

    create

---
+++ SignalRDemo/Core.SignalRDemo/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.SignalRDemo
+{
+    public class ChatHub :Hub
+    {
+        public Task SendMsg(ChatMessageInfo info)
+        {
+            return Clients.All.SendAsync("Show", $"{info.UserName}:{info.Message}");
+        }
+    }
+
+    public class ChatMessageInfo
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ SignalRDemo/SignalRDemo/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMsg(ChatMessageInfo info)
+        {
+            return Clients.All.SendAsync("Show", info.UserName + ":" + info.Message);
+        }
+    }
+
+    public class ChatMessageInfo
+    {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+    }
+}
